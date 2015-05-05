10fb652c3a48eef29fb2e367a4ab8d81d213b7f4
asyamincheva <asya.m.mincheva@gmail.com>
2015-05-05 23:56:39 +0300

    added classes; edited Player

---
+++ Minesweeper/Minesweeper.Core/Player.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper.Core
+{
+    public class Player : System.IComparable<Player>
+    {
+        private string name;
+        private int score;
+
+        public Player(string name, int score)
+        {
+            this.Name = name;
+            this.Score = score;
+        }
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+        public int Score
+        {
+            get
+            {
+                return this.score;
+            }
+            set
+            {
+                this.score = value;
+            }
+        }
+
+        public int CompareTo(Player other)
+        {
+            return this.Score.CompareTo(other.Score);
+        }
+        public override string ToString()
+        {
+            return this.name + " --> " + this.score;
+        }
+        
+    }
+}
